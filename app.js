cb800ca931c978b130ad73160305ae634e5cf33b
Slahdar <mchostak@hotmail.fr>
2024-07-10 10:18:03 +0200

    allow origin modification

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/flashArt?retryWrit
 
 // Configuration CORS
 const corsOptions = {
-    origin: 'http://localhost:5173', // Remplacez par l'origine de votre frontend
+    origin: 'http://localhost:8081',
     credentials: true, // Permettre les cookies et les en-têtes d'authentification
 };
 app.use(cors(corsOptions));
